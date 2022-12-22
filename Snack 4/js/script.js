488e09fcb5ddbc6f2d9336f4c2c49723330ef632
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-22 19:25:55 +0100

    add function addRandomNumber

--- Snack 4/js/script.js
+++ Snack 4/js/script.js
@@ -1,18 +1,43 @@
+/** 
+ * Chiedere all'utente quanti elementi vuole includere nell'array.
+ Generare attraverso l'API (array/integers) un array contenente N numeri compresi tra 1 e 100.
+ Stampare a schermo la somma dei numeri generati.
+  */
 
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            
+            amountOfNumbers: null,
+            numbers: []
         }
     },
 
     methods: {
-        
+        //Funzione creata per provare a riempire l
+        // addNumber: function (number) {
+        //     this.numbers.push(number);
+        //     this.amountOfNumbers = null;
+        // },
+
+        addRandomNumber: function () {
+            axios.get("https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items="+this.amountOfNumbers).then( (response) => 
+            {                    
+                this.numbers = response.data.response
+            });
+
+            this.amountOfNumbers = null;
+        }
     },
 
     created() {
-        this.getRandomInvited();
+        
+        
     }
 }).mount('#app')
+
+
+
+
+// https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=5
